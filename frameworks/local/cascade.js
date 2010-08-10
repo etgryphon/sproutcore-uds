421aa5774f1be0b9ea7995c58db67dfdd4b0f805
Sean Eidemiller <sean@halcyon.cc>
2010-08-10 15:42:09 -0400

    Various cleanup, consolidation, comment changes, formatting, etc.

--- frameworks/local/cascade.js
+++ frameworks/local/cascade.js
@@ -1,67 +1,68 @@
 /*globals SCUDS */
 
-SCUDS.NotifyingCascadeDataSource = SC.CascadeDataSource.extend(
-  /** @scope SCUDS.localSource.prototype */ {
-  
+SCUDS.NotifyingCascadeDataSource = SC.CascadeDataSource.extend({
+
   /**
-    A Set of dataSources that want to know about query Loads.
-  */
+   * An SC.Set of data sources that want to know about record loads.
+   */
   wantsNotification: SC.Set.create(),
 
+  /**
+   * Called when a record is loaded into the store.
+   */
   notifyDidLoadRecord: function(store, recordType, dataHash, id) {
-    var toNotify = this.wantsNotification,
-        primaryKey;
-    // console.log("***Cascade.notifyDidLoadRecord");
-    // save lookup info
+    var toNotify = this.wantsNotification;
+    var pk;
+
     recordType = recordType || SC.Record;
-    if(!id) {
-      primaryKey = recordType.prototype.primaryKey ;
-      id = dataHash[primaryKey] ;
+
+    if (!id) {
+      pk = recordType.prototype.primaryKey;
+      id = dataHash[pk];
     }
 
     toNotify.forEach(function(source) {
-      // console.log('notifying Datasource %@ %@ %@'.fmt(recordType.toString(), primaryKey, id));
-      source.notifyDidLoadRecord(store, recordType, dataHash, id) ;
-    }) ;
-
+      source.notifyDidLoadRecord(store, recordType, dataHash, id);
+    });
   },
-  
+ 
+  /**
+   * Called when a record is created in the store.
+   */
   notifyDidCreateRecord: function(store, recordType, dataHash, id) {
-    var toNotify = this.wantsNotification,
-        primaryKey;
-    console.log("***Cascade.notifyDidCreateRecord");
-    // save lookup info
+    var toNotify = this.wantsNotification;
+    var pk;
+
     recordType = recordType || SC.Record;
-    if(!id) {
-      primaryKey = recordType.prototype.primaryKey ;
-      id = dataHash[primaryKey] ;
+
+    if (!id) {
+      pk = recordType.prototype.primaryKey;
+      id = dataHash[pk];
     }
 
     toNotify.forEach(function(source) {
-      // console.log('notifying Datasource %@ %@ %@'.fmt(recordType.toString(), primaryKey, id));
-      source.notifyDidCreateRecord(store, recordType, dataHash, id) ;
-    }) ;
+      source.notifyDidCreateRecord(store, recordType, dataHash, id);
+    });
   },
-  
+
   from: function(dataSource) {
     var ret = sc_super();
-    if (dataSource.wantsNotification) this.wantsNotification.add(dataSource) ;
-    return ret ;
+    if (dataSource.wantsNotification) this.wantsNotification.add(dataSource);
+    return ret;
   },
   
   init: function() {
     sc_super();
     
-    // if a dataSources array is defined, look for any that want notifcation,
+    // If a dataSources array is defined, look for any that want notifcation,
     // then add them to our set.
-    var sources = this.get('dataSources'),
-        idx     = sources ? sources.get('length') : 0,
-        source;
-    while(--idx>=0) {
+    var sources = this.get('dataSources');
+    var idx = sources ? sources.get('length') : 0;
+    var source;
+
+    while (--idx >= 0) {
       source = sources[idx];
-      if (source.wantsNotification) this.wantsNotification.add(source) ;
+      if (source.wantsNotification) this.wantsNotification.add(source);
     }
-    
   }
-  
-}) ;
+});
