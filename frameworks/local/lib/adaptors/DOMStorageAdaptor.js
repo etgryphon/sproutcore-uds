ade642332cda0bd8fc0b4b331ef9fc7cceb089cb
Sean Eidemiller <sean@halcyon.cc>
2010-08-16 12:25:41 -0400

    Various formatting and comment changes ahead of refactoring.

--- frameworks/local/lib/adaptors/DOMStorageAdaptor.js
+++ frameworks/local/lib/adaptors/DOMStorageAdaptor.js
@@ -1,22 +1,27 @@
 /**
  * DOMStorageAdaptor
- * ===================
+ *
  * DOM Storage implementation for Lawnchair.
  *
- * - originally authored by Joseph Pecoraro
- * - window.name code courtesy Remy Sharp: http://24ways.org/2009/breaking-out-the-edges-of-the-browser
+ * Originally authored by Joseph Pecoraro.
  *
+ * window.name code courtesy Remy Sharp:
+ *  http://24ways.org/2009/breaking-out-the-edges-of-the-browser
+ *
+ * Significant portions rewritten by Sean Eidemiller (sean@halcyon.cc) to make use of intermediary
+ * lookup tables for better performance in Firefox when working with large data sets.
  */
 var DOMStorageAdaptor = function(options) {
   for (var i in LawnchairAdaptorHelpers) {
     this[i] = LawnchairAdaptorHelpers[i];
   }
+
   this.init(options);
 };
 
-
 DOMStorageAdaptor.prototype = {
-  init:function(options) {
+
+  init: function(options) {
     var self = this;
     this.storage = this.merge(window.localStorage, options.storage);
     this.table = this.merge('field', options.table);
@@ -46,7 +51,7 @@ DOMStorageAdaptor.prototype = {
     }
   },
 
-  save:function(obj, callback) {
+  save: function(obj, callback) {
     var id = this.table + '::' + (obj.key || this.uuid());
     delete obj.key;
     this.storage.setItem(id, this.serialize(obj));
@@ -56,39 +61,40 @@ DOMStorageAdaptor.prototype = {
     }
   },
 
-    get:function(key, callback) {
-        var obj = this.deserialize(this.storage.getItem(this.table + '::' + key));
-        if (obj) {
-            obj.key = key;
-            if (callback) callback(obj);
-        } else {
+  get: function(key, callback) {
+    var obj = this.deserialize(this.storage.getItem(this.table + '::' + key));
+    if (obj) {
+      obj.key = key;
+      if (callback) callback(obj);
+    } else {
       if (callback) callback(null);
     }
-    },
+  },
 
-  all:function(callback) {
+  all: function(callback) {
     var cb = this.terseToVerboseCallback(callback);
     var results = [];
     var id, tbl, key, obj;
+
     for (var i = 0, l = this.storage.length; i < l; ++i) {
       id = this.storage.key(i);
       tbl = id.split('::')[0];
       key = id.split('::').slice(1).join("::");
+
       if (tbl == this.table) {
         obj = this.deserialize(this.storage.getItem(id));
         obj.key = key;
         results.push(obj);
       }
     }
-    if (cb)
-      cb(results);
+
+    if (cb) cb(results);
   },
 
   remove:function(keyOrObj, callback) {
     var key = this.table + '::' + (typeof keyOrObj === 'string' ? keyOrObj : keyOrObj.key);
     this.storage.removeItem(key);
-    if(callback)
-      callback();
+    if (callback) callback();
   },
 
   nuke:function(callback) {
@@ -97,8 +103,8 @@ DOMStorageAdaptor.prototype = {
       for (var i = 0, l = r.length; i < l; i++) {
         self.remove(r[i]);
       }
-      if(callback)
-        callback();
+
+      if (callback) callback();
     });
   }
 };
