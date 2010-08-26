a0f2d1ddb1bf1a8b0297980d299643ce59f4659c
Sean Eidemiller <sean@halcyon.cc>
2010-08-26 10:03:31 -0400

    Whitespace and readability fixes.

--- frameworks/local/lib/Lawnchair.js
+++ frameworks/local/lib/Lawnchair.js
@@ -1,6 +1,7 @@
 sc_require('lib/adaptors/WebkitSQLiteAdaptor');
 sc_require('lib/adaptors/OrionWebkitSQLiteAdapter');
 sc_require('lib/adaptors/OrionDOMStorageAdapter');
+
 /**
  * Lawnchair
  * =========
@@ -8,73 +9,70 @@ sc_require('lib/adaptors/OrionDOMStorageAdapter');
  *
  */
 var Lawnchair = function(opts) {
-	this.init(opts);
+  this.init(opts);
 };
 
 Lawnchair.prototype = {
-	
-	init:function(opts) {
-		var adaptors = {
-			'webkit':window.WebkitSQLiteAdaptor,
-                        'orion_webkit':window.OrionWebkitSQLiteAdapter,
-			'dom':window.DOMStorageAdaptor,
-                        'orion_dom':window.OrionDOMStorageAdapter,
-			'cookie':window.CookieAdaptor,
-			'userdata':window.UserDataAdaptor
-		};
-	
-		this.adaptor = opts.adaptor ? new adaptors[opts.adaptor](opts) : new WebkitSQLiteAdaptor(opts);
-	},
-	
-	// Save an object to the store. If a key is present then update. Otherwise create a new record.
-	save:function(obj, callback) {this.adaptor.save(obj, callback);},
-	
-	// Invokes a callback on an object with the matching key.
-	get:function(key, callback) {this.adaptor.get(key, callback);},
 
-	// Returns whether a key exists to a callback.
-	exists:function(callback) {this.adaptor.exists(callback);},
-	
-	// Returns all rows to a callback.
-	all:function(callback) {this.adaptor.all(callback);},
-	
-	// Removes a json object from the store.
-	remove:function(keyOrObj, callback) {this.adaptor.remove(keyOrObj, callback);},
-	
-	// Removes all documents from a store and returns self.
-	nuke:function(callback) {this.adaptor.nuke(callback);return this;},
-	
-	/**
-	 * Iterator that accepts two paramters (methods or eval strings):
-	 *
-	 * - conditional test for a record
-	 * - callback to invoke on matches
-	 *
-	 */
-	find:function(condition, callback) {
-		var is = (typeof condition === 'string') ? function(r){return eval(condition);} : condition;
-		var cb = this.adaptor.terseToVerboseCallback(callback);
-	
-		this.each(function(record, index) {
-			if (is(record)) cb(record, index); // thats hot
-		});
-	},
+  init: function(opts) {
+    var adaptors = {
+      'webkit':window.WebkitSQLiteAdaptor,
+      'orion_webkit':window.OrionWebkitSQLiteAdapter,
+      'dom':window.DOMStorageAdaptor,
+      'orion_dom':window.OrionDOMStorageAdapter,
+      'cookie':window.CookieAdaptor,
+      'userdata':window.UserDataAdaptor
+    };
 
+    this.adaptor = opts.adaptor ? new adaptors[opts.adaptor](opts) : new WebkitSQLiteAdaptor(opts);
+  },
 
-	/**
-	 * Classic iterator.
-	 * - Passes the record and the index as the second parameter to the callback.
-	 * - Accepts a string for eval or a method to be invoked for each document in the collection.
-	 */
-	each:function(callback) {
-		var cb = this.adaptor.terseToVerboseCallback(callback);
-		this.all(function(results) {
-			var l = results.length;
-			for (var i = 0; i < l; i++) {
-				cb(results[i], i);
-			}
-		});
-	}
-// --
+  // Save an object to the store. If a key is present then update. Otherwise create a new record.
+  save: function(obj, callback) { this.adaptor.save(obj, callback); },
+
+  // Invokes a callback on an object with the matching key.
+  get: function(key, callback) { this.adaptor.get(key, callback); },
+
+  // Returns whether a key exists to a callback.
+  exists: function(callback) { this.adaptor.exists(callback); },
+
+  // Returns all rows to a callback.
+  all: function(callback) { this.adaptor.all(callback); },
+
+  // Removes a json object from the store.
+  remove: function(keyOrObj, callback) { this.adaptor.remove(keyOrObj, callback); },
+
+  // Removes all documents from a store and returns self.
+  nuke: function(callback) { this.adaptor.nuke(callback); return this; },
+
+  /**
+   * Iterator that accepts two paramters (methods or eval strings):
+   *
+   * - conditional test for a record
+   * - callback to invoke on matches
+   */
+  find: function(condition, callback) {
+    var is = (typeof condition === 'string') ? function(r) { return eval(condition); } : condition;
+    var cb = this.adaptor.terseToVerboseCallback(callback);
+
+    this.each(function(record, index) {
+      if (is(record)) cb(record, index); // thats hot
+    });
+  },
+
+  /**
+   * Classic iterator.
+   * - Passes the record and the index as the second parameter to the callback.
+   * - Accepts a string for eval or a method to be invoked for each document in the collection.
+   */
+  each: function(callback) {
+    var cb = this.adaptor.terseToVerboseCallback(callback);
+    this.all(function(results) {
+      var l = results.length;
+      for (var i = 0; i < l; i++) {
+        cb(results[i], i);
+      }
+    });
+  }
 };
 
