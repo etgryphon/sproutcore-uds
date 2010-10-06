cc9f5585355e4925ca1bdb9f9397050e0ff8ede5
Sean Eidemiller <sean@halcyon.cc>
2010-10-06 09:51:18 -0400

    Rework of adapter classes.

--- frameworks/local/storage/dom.js
+++ frameworks/local/storage/dom.js
@@ -1,65 +1,60 @@
-/*globals SCUDS*/
+/* globals SCUDS */
 
-/** @class
-  
-  Little wrapper for DOM storage
-  @extends SC.Object
-  @author Mike Ball
-  @author Sean Eidemiller
-  @version 0.1
-*/
-SCUDS.DOMStorageAdapter = SC.Object.extend(
-/** @scope SCUDS.DOMStorageAdapter.prototype */{
-  
-  //usually the record type
+sc_require('storage/adapter');
+
+/**
+ * Storage adapter for DOM storage. 
+ *
+ * @extends SCUDS.LocalStorageAdapter
+ * @author Mike Ball
+ * @author Sean Eidemiller
+ *
+ * @version 0.1
+ */
+SCUDS.DOMStorageAdapter = SCUDS.LocalStorageAdapter.extend(
+  /** @scope SCUDS.DOMStorageAdapter.prototype */{
+ 
   localStorageKey: '',
-  
-  //unique idenitify on the record usually id or guid
   contentItemKey: 'id',
-  
-  shouldBenchmark: NO,
-  
-  _deserializeHash: function(){
-    if(this.shouldBenchmark) SC.Benchmark.start('domAccess');
+
+  _deserializeHash: function() {
     var key = this.localStorageKey, results;
+
     try {
       results = SC.json.decode(window.localStorage.getItem(key)) || {};
     } catch(e) {
-      console.warn('Error during deserialization of records; clearing the cache.');
+      SC.Logger.warn('Error during deserialization of records; clearing the cache.');
       this.nuke();
       results = {};
     }
-    if(this.shouldBenchmark) SC.Benchmark.end('domAccess');
+
     return results;
   },
-  
-  
+
   _deserializeAsArray: function(){
-    if(this.shouldBenchmark) SC.Benchmark.start('arrayAccess');
     var key = this.localStorageKey, results;
+
     try {
       var data = window.localStorage.getItem(key) || '';
-      data = data.replace(/\"[0-9]+\":/gi,''); //turn it into an array reduce lines for ie...
-      results = SC.json.decode('['+data.substring(1,data.length-1)+']') || [];
+      data = data.replace(/\"[0-9]+\":/gi,'');
+      results = SC.json.decode('[' + data.substring(1, data.length - 1) + ']') || [];
     } catch(e) {
-      console.warn('Error during deserialization of records; clearing the cache.');
+      SC.Logger.warn('Error during deserialization of records; clearing the cache.');
       this.nuke();
       results = [];
     }
-    if(this.shouldBenchmark) SC.Benchmark.end('arrayAccess');
+
     return results;
   },
-  //TODO: to make this faster...
-  // •compress data
-  // •put writes into pages
-  // •only write onece per browser session (at window close)
+
+  // TODO: [SE, MB] To make this faster...
+  // - Compress data [SE: This will likely make it slower]
+  // - Put writes into pages
+  // - Only write onece per browser session (at window close) [SE: This is dangerous]
   _serializeHash: function(data){
-    if(this.shouldBenchmark) SC.Benchmark.start('encodingData');
     var ret = window.localStorage.setItem(this.localStorageKey, SC.json.encode(data));
-    if(this.shouldBenchmark) SC.Benchmark.end('encodingData');
     return ret;
   },
-  
 
   /**
    * Writes a single record or an array of records to the local storage.
@@ -71,12 +66,9 @@ SCUDS.DOMStorageAdapter = SC.Object.extend(
    */
   save: function(obj, key) {
     if (obj instanceof Array) {
-      // Got an array of objects.
       return this._saveAll(obj);
-    }
-
-    if (!obj) {
-      console.error('Error writing record to cache: Record is null or undefined.');
+    } else if (!obj) {
+      SC.Logger.error('Error writing record to cache: Record is null or undefined.');
       return NO;
     }
 
@@ -88,7 +80,7 @@ SCUDS.DOMStorageAdapter = SC.Object.extend(
       this._serializeHash(data);
       return YES;
     } else {
-      console.error('Error writing record to cache: Invalid key.');
+      SC.Logger.error('Error writing record to cache: Invalid key.');
       return NO;
     }
   },
@@ -96,79 +88,72 @@ SCUDS.DOMStorageAdapter = SC.Object.extend(
   /**
    * Writes multiple records to the local storage.
    *
-   * TODO: [MB] Return YES|NO to pass along to return call from save().
+   * TODO: [MB] Return YES | NO to pass along to return call from save().
    */
   _saveAll: function(array) {
     var data = this._deserializeHash();
     var length = array.length;
-    if(length > 10 && SC.browser.msie){ //only do this for msie
+
+    if (length > 10 && SC.browser.msie) {
       var startIndex = 0, that = this;
-        setTimeout(function(){
-          that._chunkSave(data, array, startIndex, length);
-        }, 150);
-    }
-    else{
+      setTimeout(function() { that._chunkSave(data, array, startIndex, length); }, 150);
+    } else {
       for (var i = 0; i < length; i++) {
-        data[array[i][this.contentItemKey]] = array[i]; //TODO: [MB] optimize ie perf
+        data[array[i][this.contentItemKey]] = array[i];
       }
+
       this._serializeHash(data);
     }
   },
-  
-  _chunkSave: function(data, array, startIndex, length){
-    var i, newStartIndex = startIndex+10;
-    if(newStartIndex < length){
-      for(i = newStartIndex-10; i < newStartIndex; i++){
+
+  _chunkSave: function(data, array, startIndex, length) {
+    var i, newStartIndex = startIndex + 10;
+
+    if (newStartIndex < length) {
+      for (i = newStartIndex - 10; i < newStartIndex; i++){
         data[array[i][this.contentItemKey]] = array[i]; 
       }
+
       var that = this;
-      setTimeout(function(){
-        that._chunkSave(data,array,newStartIndex,length);
-      }, 150);
-    }
-    else{
-      for(i = newStartIndex-10; i < length; i++){
+      setTimeout(function() { that._chunkSave(data,array,newStartIndex,length); }, 150);
+    } else {
+      for(i = newStartIndex - 10; i < length; i++){
         data[array[i][this.contentItemKey]] = array[i]; 
       }
+
       this._serializeHash(data);
     }    
   },
 
   /**
-   Reads a single record from the local storage.
-  */
+   * Reads a single record from the local storage.
+   */
   get: function(id) {
     var data = this._deserializeHash();
     var rec = data[id];
-
     return rec ? rec : null;
   },
 
   /**
-    Reads all of the records in this table from the local storage.
-  */
+   * Reads all of the records in this table from the local storage.
+   */
   getAll: function() {
     return this._deserializeAsArray();
   },
 
   /**
-    Removes a single record from the local storage.
-  */
+   * Removes a single record from the local storage.
+   */
   remove: function(id) {
     var data = this._deserializeHash();
     delete data[id];
-    
     this._serializeHash(data);
   },
 
   /**
-    Removes all data associated with this table from the local storage.
-  */
+   * Removes all data associated with this table from the local storage.
+   */
   nuke: function() {
     window.localStorage.removeItem(this.localStorageKey);
-
   }
-  
-  
-  
 });
